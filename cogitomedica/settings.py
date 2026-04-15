6301c88b0761b6b3dd1d25e2828089e9accd56d8
Piotr Popiołek <piotr.popiolek93@gmail.com>
2026-04-15 20:09:02 +0200

    Refactor password validators in settings.py for improved security
    - Uncommented and structured the AUTH_PASSWORD_VALIDATORS list to enable essential password validation features in Django.
    - Ensured that the validators for user attribute similarity, minimum length, common passwords, and numeric passwords are now active, enhancing password security measures.

--- cogitomedica/settings.py
+++ cogitomedica/settings.py
@@ -633,18 +633,18 @@ DATABASES = {
 
 
 AUTH_PASSWORD_VALIDATORS: list[dict[str, str]] = [
-    # {
-    #     "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    # },
-    # {
-    #     "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    # },
-    # {
-    #     "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    # },
-    # {
-    #     "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    # },
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
